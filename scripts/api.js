eb51128b753c024bde761d8134512daba1361350
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-04-11 14:50:35 -0500

    Add createItem to api

--- scripts/api.js
+++ scripts/api.js
@@ -2,11 +2,23 @@
 /* global $ */
 
 const api = (() => {
-	const BASE_URL = 'https://thinkful-list-api.herokuapp.com/terrance-corley';
+	const BASE_URL = 'https://thinkful-list-api.herokuapp.com/terrance-jeffrey';
 
 	function getItems(callback) {
 		$.getJSON(`${BASE_URL}/items`, callback);
 	}
 
-	return {getItems};
+	function createItem(name, callback) {
+		let newItem = JSON.stringify({ name: name });
+
+		$.ajax({
+			url: `${BASE_URL}/items`,
+			method: 'POST',
+			contentType: 'application/json',
+			data: newItem,
+			success: callback
+		});
+	}
+
+	return { getItems, createItem };
 })();
